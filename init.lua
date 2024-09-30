879ddf7746cf8822c898f81b16a55f4e89424990
aris <aris.mav@hotmail.com>
2024-09-30 21:28:59 +0200

    initial commit

---
+++ init.lua
@@ -0,0 +1,36 @@
+-- Get the lazy plugins
+require("config.lazy")
+
+-- Set colors
+vim.cmd[[colorscheme desert]]
+
+-- Remove border between vertical windows
+vim.cmd[[:hi VertSplit ctermfg=bg ctermbg=bg guifg=bg guibg=bg]]
+vim.cmd[[set fillchars+=vert:\ ]]
+
+-- Indentations
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+-- Slime thing (if plugin is not available)
+-- vim.api.nvim_set_keymap('v', '<cr>', 'y<C-w>wpi<cr><C-\\><C-N><C-w>w', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<cr>', 'Y<C-w>wpi<cr><C-\\><C-N><C-w>w', { noremap = true, silent = true })
+
+-- Keybinds --
+
+-- Open terminal window wiht Alt-t
+vim.keymap.set("n", "<A-t>", ":vert term <cr> <C-w>w", { remap = true, silent = false })
+
+-- Swap windows using alt+hjkl
+vim.keymap.set("n", "<A-h>", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-j>", "<C-w>j", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-k>", "<C-w>k", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-l>", "<C-w>l", { noremap = true, silent = true })
+-- Same for terminal mode
+vim.keymap.set("t", "<A-h>", "<C-\\><C-N><C-w>h", { noremap = true, silent = true })
+vim.keymap.set("t", "<A-j>", "<C-\\><C-N><C-w>j", { noremap = true, silent = true })
+vim.keymap.set("t", "<A-k>", "<C-\\><C-N><C-w>k", { noremap = true, silent = true })
+vim.keymap.set("t", "<A-l>", "<C-\\><C-N><C-w>l", { noremap = true, silent = true })
+
