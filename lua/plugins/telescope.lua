73d26a55d542c6826f0b08bbafb75de30dbcc632
aris <aristarchos.mavridis@manchester.ac.uk>
2024-10-17 16:54:17 +0100

    added lspzero

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,17 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     -- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+
+    require('telescope').setup{
+        defaults = {
+            -- Default configuration for telescope goes here:
+            -- config_key = value,
+            mappings = {
+                n = {
+                    ['dd'] = require('telescope.actions').delete_buffer
+                }, -- n
+            } -- mappings
+        }, -- defaults
+    }
 }
