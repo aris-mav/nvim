9863f092bb9833c0257f7daf223a507c7ed23644
aris <aristarchos.mavridis@manchester.ac.uk>
2025-02-07 15:40:50 +0000

    updates on gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,6 +3,7 @@ return {
     config = function ()
         vim.opt.signcolumn = "yes"
         vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+        vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
         require('gitsigns').setup {
             signs = {
                 add          = { text = '┃' },
@@ -29,12 +30,12 @@ return {
                 follow_files = true
             },
             auto_attach = true,
-            attach_to_untracked = false,
+            attach_to_untracked = true,
             current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
             current_line_blame_opts = {
                 virt_text = true,
                 virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-                delay = 1000,
+                delay = 10,
                 ignore_whitespace = false,
                 virt_text_priority = 100,
                 use_focus = true,
@@ -43,7 +44,7 @@ return {
             sign_priority = 6,
             update_debounce = 100,
             status_formatter = nil, -- Use default
-            max_file_length = 40000, -- Disable if file is longer than this (in lines)
+            max_file_length = 5000, -- Disable if file is longer than this (in lines)
             preview_config = {
                 -- Options passed to nvim_open_win
                 border = 'single',
