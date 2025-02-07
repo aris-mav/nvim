6cadc268cb1ed3f33d0bcef0b2bf686c03997115
arismav <aris.mav@hotmail.com>
2025-02-07 18:13:34 +0000

    remove terminal signcolumn

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -42,6 +42,7 @@ vim.opt.smartcase = true
 vim.api.nvim_create_autocmd('TermOpen',{
     group = vim.api.nvim_create_augroup('custom-term-open', {clear = true }),
     callback = function()
+        vim.opt.signcolumn = "no"
         vim.opt.number = false
         vim.opt.relativenumber = false
     end,
